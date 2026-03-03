0dfe512215a9418c27a8220d9bfad85294902974
Victor Horobchuk <Gorobchuk333@gmail.com>
2026-03-03 13:00:14 +0200

    feat: init setup

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome!"))
+	})
+
+	r.Get("/users/{userID}", func(w http.ResponseWriter, r *http.Request) {
+		userID := chi.URLParam(r, "userID")
+		w.Write([]byte("Fetching user: " + userID))
+	})
+
+	log.Println("Server starting on port :3000")
+	log.Fatalln(http.ListenAndServe(":3000", r))
+}
