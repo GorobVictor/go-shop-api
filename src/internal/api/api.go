7567ffcb6369d360964a654c606417f901750585
Victor Horobchuk <Gorobchuk333@gmail.com>
2026-03-06 18:20:48 +0200

    feat: add logic for auth

--- src/internal/api/api.go
+++ src/internal/api/api.go
@@ -3,15 +3,24 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
+	"shop-api/internal/api/routes"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 func Run() {
+	secret := os.Getenv("JWT_SECRET")
 
+	if secret == "" {
+		log.Fatalln("JWT secret is null")
+	}
+
+	tokenAuth := jwtauth.New("HS256", []byte(secret), nil)
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -22,6 +31,8 @@ func Run() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome!"))
 	})
+
+	routes.Users(r, tokenAuth)
 
 	log.Println("Server starting on port :3000")
 	log.Fatalln(http.ListenAndServe(":3000", r))
