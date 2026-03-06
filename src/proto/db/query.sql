6709b83f52111dcd0615b8f2a9cef6ca6d320015
Victor Horobchuk <Gorobchuk333@gmail.com>
2026-03-06 18:19:57 +0200

    feat: update queries to db

--- src/proto/db/query.sql
+++ src/proto/db/query.sql
@@ -5,8 +5,14 @@ INSERT INTO public.users(first_name, last_name, email, password_hash, user_role)
 -- name: GetUser :one
 SELECT * FROM users WHERE id = $1 limit 1;
 
+-- name: GetUserProfile :one
+SELECT ID, first_name, last_name, email, user_role, created_at FROM users WHERE id = $1 limit 1;
+
 -- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1 limit 1;
 
 -- name: AnyEmail :one
 SELECT email FROM users WHERE email = $1 limit 1;
+
+-- name: GetUsers :many
+Select ID, first_name, last_name, email, user_role, created_at from users order by id limit $1 offset $2;
